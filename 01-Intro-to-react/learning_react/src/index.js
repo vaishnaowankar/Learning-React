dad9cbaf3e11a2bdadb160dcf86773c0dc5a14f1
vaishnaowankar <vaishnao.wankar2000@gmail.com>
2023-04-23 11:13:45 +0530

    Created a greeting text app

--- 01-Intro-to-react/learning_react/src/index.js
+++ 01-Intro-to-react/learning_react/src/index.js
@@ -4,12 +4,38 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// Example of changing the greeting text according to current time.
+const cssStyle = {};
+
+let greetingText = "";
+
+let currDate = new Date();
+let currHour = currDate.getHours();
+
+if (currHour >= 0 && currHour <= 11) {
+  greetingText = "Good Morning";
+  cssStyle.color = "green"; // adding a key value pair to object and manipulating accordingly
+} else if (currHour >= 12 && currHour <= 19) {
+  greetingText = "Good Evening";
+  cssStyle.color = "orange";
+} else {
+  greetingText = "Good Night";
+  cssStyle.color = "violet";
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
-  <h1>Hello World!</h1>
+  // Following is a React.Fragment way of rendering multiple elements , just use the starting and ending tags and put the elements in between them
+  <>
+    <div>
+      <h1>
+        Hello Sir , <span style={cssStyle}>{greetingText}</span>{" "}
+      </h1>
+    </div>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
